dd4362c7d60f3ca0da632c741b2afa425775a0c0
ovalleluis <luisovalle@ip-10-10-0-142.ap-southeast-2.compute.internal>
2015-10-23 18:25:23 +1100

    Refactoring Supplier & Customer as subclasses of Company Parent Class

--- Site/core/admin.py
+++ Site/core/admin.py
@@ -5,5 +5,8 @@ from django.contrib import admin
 from django.contrib import admin
 
 from .models import Supplier
+from .models import Customer
+
 
 admin.site.register(Supplier)
+admin.site.register(Customer)
--- Site/core/models.py
+++ Site/core/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class Supplier(models.Model):
-    company_representative_name = models.CharField(max_length=200)
-    company_name = models.CharField(max_length=200)
-    company_zipcode = models.CharField(max_length=20)
-    company_phone = models.CharField(max_length=20) 
-    company_email = models.EmailField()
-    password = models.CharField(max_length=100)   
+class Company(models.Model):
+	company_representative_name = models.CharField(max_length=200)
+	company_name = models.CharField(max_length=200)
+	company_zipcode = models.CharField(max_length=20)
+	company_phone = models.CharField(max_length=20) 
+	company_email = models.EmailField()
+	password = models.CharField(max_length=100)   
+	class Meta:
+        	abstract = True
 
+class Supplier(Company):
+	pass
+
+class Customer(Company):
+	pass
