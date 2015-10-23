4442d5ef0641c0e60c2e593150b9e8f6768cd88f
ovalleluis <luisovalle@ip-10-10-0-142.ap-southeast-2.compute.internal>
2015-10-23 18:23:23 +1100

    Refactoring Supplier & Customer as subclasses of Company Parent Class

---
+++ Site/core/migrations/0002_customer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Customer',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('company_representative_name', models.CharField(max_length=200)),
+                ('company_name', models.CharField(max_length=200)),
+                ('company_zipcode', models.CharField(max_length=20)),
+                ('company_phone', models.CharField(max_length=20)),
+                ('company_email', models.EmailField(max_length=254)),
+                ('password', models.CharField(max_length=100)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+    ]
