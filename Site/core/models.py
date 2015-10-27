3a61185e8d3b26e85cefddc6bc6707bd5d22e30d
ovalleluis <luisovalle@ip-10-10-0-142.ap-southeast-2.compute.internal>
2015-10-27 16:57:55 +1100

    Rfp and related models added

--- Site/core/models.py
+++ Site/core/models.py
@@ -8,6 +8,7 @@ class Company(models.Model):
 	company_zipcode = models.CharField(max_length=20)
 	company_phone = models.CharField(max_length=20) 
 	company_email = models.EmailField()
+	created_on = models.DateTimeField(auto_now_add=True)
 	password = models.CharField(max_length=100)   
 	class Meta:
         	abstract = True
@@ -17,3 +18,26 @@ class Supplier(Company):
 
 class Customer(Company):
 	pass
+
+class Rfp(models.Model):
+	customer = models.OneToOneField(Customer)
+	created_on = models.DateTimeField(auto_now_add=True)
+
+
+
+class RfpRequirement(models.Model):
+	ask = models.CharField(max_length=200)
+	rfp = models.ForeignKey(Rfp)
+
+
+
+class Pfr(models.Model):
+	created_on = models.DateTimeField(auto_now_add=True)
+	rfp = models.ForeignKey(Rfp)
+
+
+
+class PfrResponse(models.Model):
+	bid = models.CharField(max_length=200)
+	pfr = models.ForeignKey(Pfr)
+	rfpRequirement = models.ForeignKey(RfpRequirement)
