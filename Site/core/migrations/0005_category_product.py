09363abb9f8bbe623f99e29dcbf78abee4791f74
ovalleluis <luisovalle@ip-10-10-0-100.ap-southeast-2.compute.internal>
2015-11-05 11:55:17 +1100

    Adding Category and Product entities

---
+++ Site/core/migrations/0005_category_product.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0004_auto_20151027_0630'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('parent_category', models.ForeignKey(related_name='categories', blank=True, to='core.Category', null=True)),
+            ],
+            options={
+                'verbose_name_plural': 'categories',
+            },
+        ),
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('is_service', models.BooleanField(default=False)),
+                ('category', models.ForeignKey(related_name='products', to='core.Category')),
+            ],
+        ),
+    ]
